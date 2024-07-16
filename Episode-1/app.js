2916c2da3460311d25fbbaf605559970ddbe1ba0
farhankhan0511 <farhankhan051104@gmail.com>
2024-07-16 18:22:28 +0530

    episode1

---
+++ Episode-1/app.js
@@ -0,0 +1,28 @@
+const parent=React.createElement("div",{id:"parent"},
+    [React.createElement("div",{id:"child"},
+        [React.createElement("h1",{id:"1"},"Fuck off"),React.createElement("h1",{id:"2"},"Bitch")]
+    ),
+    React.createElement("div",{id:"child2"},
+        [React.createElement("h1",{id:"12"},"Fuck off"),React.createElement("h1",{id:"22"},"Bitch")]
+    )]
+    )
+
+
+
+
+console.log(parent);
+
+
+
+
+
+
+
+
+
+
+
+
+const heading=React.createElement("h1",{id:"head"},"Helloworld from React!")
+    const root=ReactDOM.createRoot(document.getElementById("root"))
+    root.render(parent);
